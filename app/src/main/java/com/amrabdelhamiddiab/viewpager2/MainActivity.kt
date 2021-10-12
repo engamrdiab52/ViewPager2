b9041fadda9547092f890c8c636b0c3e66f60018
engam <engamrdiab52@gmail.com>
2021-10-12 22:15:49 +0200

    add card resize

--- app/src/main/java/com/amrabdelhamiddiab/viewpager2/MainActivity.kt
+++ app/src/main/java/com/amrabdelhamiddiab/viewpager2/MainActivity.kt
@@ -3,11 +3,14 @@ package com.amrabdelhamiddiab.viewpager2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
+import com.tbuonomo.viewpagerdotsindicator.SpringDotsIndicator
 import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator
+import java.lang.Math.abs
 
 class MainActivity : AppCompatActivity() {
     companion object{
@@ -40,10 +43,34 @@ class MainActivity : AppCompatActivity() {
 
         viewPager2 = findViewById(R.id.view_pager)
         viewPager2.adapter = adapter
-        val dotsIndicator = findViewById<DotsIndicator>(R.id.dots_indicator)
+        val dotsIndicator = findViewById<SpringDotsIndicator>(R.id.spring_dots_indicator)
         dotsIndicator.setViewPager2(viewPager2)
         viewPager2.clipToPadding = false
         viewPager2.setPadding(0, 0, 0, 0)
-      //  viewPager2.requestTransform()
+      ////////////////////////////////////////////////
+        // You need to retain one page on each side
+        // so that the next and previous items are visible
+        viewPager2.offscreenPageLimit = 1
+        // Add a PageTransformer that translates the next and previous items horizontally
+        // towards the center of the screen, which makes them visible
+        val nextItemVisiblePx = resources.getDimension(R.dimen.viewpager_next_item_visible)
+        val currentItemHorizontalMarginPx = resources
+            .getDimension(R.dimen.viewpager_current_item_horizontal_margin)
+        val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
+        val pageTransformer = ViewPager2.PageTransformer { page: View, position: Float ->
+            page.translationX = -pageTranslationX * position
+            // Next line scales the item's height. You can remove it if you don't want this effect
+            page.scaleY = 1 - (0.25f * abs(position))
+            // If you want a fading effect uncomment the next line:
+           //  page.alpha = 0.25f + (1 - abs(position))
+        }
+        viewPager2.setPageTransformer(pageTransformer)
+            // The ItemDecoration gives the current (centered) item horizontal margin so that
+            // it doesn't occupy the whole screen width. Without it the items overlap
+        val itemDecoration = HorizontalMarginItemDecoration(
+            this,
+            R.dimen.viewpager_current_item_horizontal_margin
+        )
+        viewPager2.addItemDecoration(itemDecoration)
     }
 }
